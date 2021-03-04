ee9a24bc55e60de201968c1c461f667205fb64c6
Santosh Reddy Tera <stera@Santoshs-MacBook-Pro.local>
2021-03-04 00:02:29 -0700

    second commit

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 ## STEPS
 * 1. Copy "main/resources/static/framework" & "main/resources/static/nested" folder to your project.
 * 2. Copy "extracthome.jsp" to your jsp folder and change the name in Controller.
+
+
+##Day 2 ---
+
+1. Update -- nested.js and nest.html in /static/nested folder. 
+2. 
--- src/main/resources/static/nested/nested.js
+++ src/main/resources/static/nested/nested.js
@@ -3,26 +3,27 @@
  */
 angular.module("demo").controller("NestedListsDemoController", function($scope) {
 
+	$scope.tableItem = [];
     $scope.models = {
         selected: null,
         templates: [
-            {name: "GetRDMS", id: 2, type: "item"},
-            {name: "ExecuteSP", id: 1, type: "item"},
+            {name: "GetRDMS", id: 1, type: "item"},
+            {name: "ExecuteSP", id: 2, type: "item"},
             {name: "ExecuteCommand", type: "item",id: 3},
             {name: "CreateCSV", id: 4, type: "item"},
-            {name: "SendEmail", id: 5, type: "item",id: 3}
+            {name: "SendEmail", id: 5, type: "item"}
         ],
         dropzones: {
             "A": [
                 {
                     "name": "GetRDMS",
                     "type": "item",
-                    "id": 2,
+                    "id": 1,
                 },
                 {
                     "name": "ExecuteSP",
                     "type": "item",
-                    "id": "1"
+                    "id": "2"
                 },
                 {
                     "name": "ExecuteCommand",
@@ -38,6 +39,12 @@ angular.module("demo").controller("NestedListsDemoController", function($scope) 
         }
     };
 
+    $scope.saveForLater = function(formItem){
+    	console.log(formItem);
+    	$scope.tableItem.push(angular.copy(formItem));
+    	this.formItem = '';
+    	
+    }
     $scope.$watch('models.dropzones', function(model) {
         $scope.modelAsJson = angular.toJson(model, true);
     }, true);
