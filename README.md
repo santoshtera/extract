1d3ff5ddad52bacf168126ddc5cb766401b00ffb
Santosh Reddy Tera <stera@Santoshs-MacBook-Pro.local>
2021-03-09 00:55:24 -0700

    Day 3 updated

--- README.md
+++ README.md
@@ -9,3 +9,10 @@
 
 1. Update -- nested.js and nest.html in /static/nested folder.  Take update of these two files.
 2. Done only to add rows to table, we will look into trash thing tomorrow. 
+
+
+## Day 3 --
+1. include these lines in home.jsp -->  
+<script src="//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.7.0underscore.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.js"></script>
+  2. update nest.hmtl, next.js
--- src/main/resources/static/nested/nested.js
+++ src/main/resources/static/nested/nested.js
@@ -7,49 +7,31 @@ angular.module("demo").controller("NestedListsDemoController", function($scope,$
     $scope.models = {
         selected: null,
         templates: [
-            {name: "GetRDMS", id: 1, type: "item"},
-            {name: "ExecuteSP", id: 2, type: "item"},
-            {name: "ExecuteCommand", type: "item",id: 3},
-            {name: "CreateCSV", id: 4, type: "item"},
-            {name: "SendEmail", id: 5, type: "item"}
+            {name: "GetRDMS",type: "item"},
+            {name: "ExecuteSP", type: "item"},
+            {name: "ExecuteCommand", type: "item"},
+            {name: "CreateCSV",type: "item"},
+            {name: "SendEmail",type: "item"}
         ],
         dropzones: {
             "A": [
-                {
+            	{
                     "name": "GetRDMS",
                     "type": "item",
-                    "id": 1,
-                },
-                {
-                    "name": "ExecuteSP",
-                    "type": "item",
-                    "id": "2"
-                },
-                {
-                    "name": "ExecuteCommand",
-                    "type": "item",
-                    "id": "3"
-                },
-                {
-                    "name": "CreateCSV",
-                    "type": "item",
-                    "id": "4"
-                }
-            ]
+                    "id": 9
+                } 
+            ] 
         }
     };
 
     $scope.saveForLater = function(formItem){
     	console.log(formItem);
     	$scope.tableItem.push(angular.copy(formItem));
-    	
     	this.formItem = '';
-    	
     }
     
-    
     var formData = {
-  		itemsInfo: $scope.tableItem
+  		itemsInfo: $scope.tableItem	
     };
     
     $scope.submit = function () {
@@ -57,12 +39,33 @@ angular.module("demo").controller("NestedListsDemoController", function($scope,$
   	  $http.post("/workOrderDetails", formData).
         success(function (response) {
       	  $scope.response = response;
+      //	this.response.formData.itemsInfo = [];
       	  console.log($scope.response);
+      	  
         }).
         error(function () {
+        	// $formData.itemsInfo = [];
             alert("Submit Form failed, please try again");
+           
         });
     };
+    
+    // var findItemById = function(items, itemId) {
+     //   return _.find(items, function(item) {
+     //     return item.itemId === itemId;
+    //    });
+   //   };
+  /**  $scope.getValuesFromTable = function (item) {
+    	$scope.models.dropzones = _.findItemById(item, item.id, function(itemid){
+  			if(itemid.isPresent()){
+  			
+  				return item;
+  			}
+  			
+  			else item = [];
+  		}
+    }); **/
+    
     $scope.$watch('models.dropzones', function(model) {
         $scope.modelAsJson = angular.toJson(model, true);
     }, true);
